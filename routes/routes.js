059f630d146f227e12d034f2d8abbdb26deba774
e.atayi <erfan.at799@gmail.com>
2023-04-14 18:07:35 +0330

    add a ch-token route

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const functions = require("../utils/functions")
+const resBuilder = require('../utils/responseBiulder')
+const moment = require('moment-jalali')
 
 const auth = require("./auth.route")
 const admin = require("./admin.route")
@@ -8,5 +10,10 @@ const user = require("./user.route")
 router.use("/auth", auth)
 router.use("/admin", functions.authentication, functions.checkUserExist, functions.checkAdminRole, admin)
 router.use("/user", functions.authentication, functions.checkUserExist, functions.checkUserRole, user)
+
+router.get("/check-token", functions.authentication, functions.checkUserExist, (req, res) => {
+    req.userData.registerDate = moment(req.userData.registerDate, 'X').format('jYYYY/jMM/jDD')
+    return resBuilder.success(res, req.userData, "")
+})
 
 module.exports = router
